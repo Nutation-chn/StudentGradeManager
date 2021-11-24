dd6522fd6e65a2c28d2aa54484a584f3218a7823
Jessie <shen0113@algonquinlive.com>
2021-11-24 17:28:59 -0500

    Edited AppLauncher

---
+++ src/studentGrade/AppLauncher.java
@@ -0,0 +1,17 @@
+package studentGrade;
+/*
+Class Name: CST8288_014
+Author Name: Dong Zhang, Feng Chen, Jessie Shen
+Class Description: AppLauncher. Main method to instantiates StudentGradeDriver and call the startApp() method
+Date: Nov. 24, 2021
+*/
+
+public class AppLauncher {
+
+	public static void main(String[] args) {
+		StudentGradeDriver stuDriver = new StudentGradeDriver();
+		stuDriver.startApp();
+
+	}
+
+}
