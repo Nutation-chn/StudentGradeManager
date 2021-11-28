969ce38f6bce8762aa619d44a9f2ecf8bc5f6062
Feng Chen <fengchen.cn@hotmail.com>
2021-11-27 19:07:12 -0500

    First Commit By Feng Chen

--- src/studentGrade/StudentDAL.java
+++ src/studentGrade/StudentDAL.java
@@ -1,5 +1,14 @@
 package studentGrade;
 
-public interface StudentDAL {
+import java.util.ArrayList;
+import java.util.List;
 
+public interface StudentDAL {
+	List<Student> getAll();
+	Student getStudent( int id);
+	boolean add(Student student);
+	void updateStudent(int id, String firstName, String lastName, String dob, String emailAddress, int midterm1,
+			int midterm2, int assignment1, int assignment2, int assignment3, int assignment4, int assignment5,
+			int finalScore, String finalGrade);
+	void deleteStudent(int id);
 }
